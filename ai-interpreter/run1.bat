dd1c7c7dd98b540a5a781ae473a5733934827d1c
texas1980 <122022646+texas1980@users.noreply.github.com>
2024-12-16 10:12:27 +0300

    Add files via upload

---
+++ ai-interpreter/run1.bat
@@ -0,0 +1,1 @@
+python Interpreter.py "Пример 1 - План-график общий_rnd.cmj" "Пример 1 - Решение ИИ.cmj"
---
+++ ai-interpreter/run2.bat
@@ -0,0 +1,1 @@
+python Interpreter.py "Пример 2 - План-график частный_rnd.cmj" "Пример 2 - Решение ИИ.cmj"
